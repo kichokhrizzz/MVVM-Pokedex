677ce9a6b1d0d7658b57ef71bcf6cc380daaaaae
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-06-05 08:49:05 -0600

    Initial Commit

---
+++ MVVM-Pokedex/MVVM_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_PokedexApp.swift
+//  MVVM-Pokedex
+//
+//  Created by Jhosel Badillo Cortes on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
